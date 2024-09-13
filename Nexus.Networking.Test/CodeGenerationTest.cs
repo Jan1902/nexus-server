7f8a5a7ac75d4cbdcd98e41db8c9d37ad5c7edfd
Jan-Hendrik Heinbokel <jan-hendrik.heinbokel@adesso.de>
2024-09-14 00:09:46 +0200

    Began implementing code generation

---
+++ Nexus.Networking.Test/CodeGenerationTest.cs
@@ -0,0 +1,20 @@
+﻿using Nexus.Networking.CodeGeneration;
+
+namespace Nexus.Networking.Test;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        var generator = new PacketSerializationGenerator();
+        var context = new Microsoft.CodeAnalysis.GeneratorExecutionContext();
+        generator.Execute(context);
+        Console.WriteLine();
+    }
+}
